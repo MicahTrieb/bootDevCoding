52407d91e2fd4c78d2ac4ce119275e5dc233596e
lowrezdragon@gmail.com <lowrezdragon@gmail.com>
2025-01-28 14:14:16 -0600

    Final commit for the day, removing duplicate files

--- dupeRemover.py
+++
@@ -1,9 +0,0 @@
-def remove_duplicates(nums):
-    returnList = []
-    for currentNum in nums:
-        if currentNum not in returnList:
-            returnList.append(currentNum)
-
-    return returnList
-        
-
--- nameCounter.py
+++
@@ -1,9 +0,0 @@
-def count_names(list_of_lists, target_name):
-    nameCounter = 0
-    for currentList in list_of_lists:
-        if target_name in currentList:
-            for currentName in currentList:
-                if currentName == target_name:
-                    nameCounter += 1
-    return nameCounter
-                
