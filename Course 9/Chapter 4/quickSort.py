263c5e7cb31fe7d19e5c665ffa96488326613ae1
lowrezdragon@gmail.com <lowrezdragon@gmail.com>
2025-01-29 14:11:30 -0600

    Starting selectionSort.py

--- Course 9/Chapter 4/quickSort.py
+++ Course 9/Chapter 4/quickSort.py
@@ -1,3 +1,5 @@
+#Lesson Link: https://www.boot.dev/lessons/5f62f856-53bf-47aa-a5b0-ada9381e3e57
+
 def quick_sort(nums, low, high):
     if low < high:
         currentPart = partition(nums, low, high)
@@ -17,4 +19,26 @@ def partition(nums, low, high):
     nums[i] = swapTwo
     nums[high] = swapOne
     return i
+
+
+#Second copy rewritten for practice 
+
+def quick_sort(nums, low, high):
+    if low < high:
+        current_part = partition(nums, low, high)
+        quick_sort(nums, low, current_part - 1)
+        quick_sort(nums, current_part, high)
+
+
+def partition(nums, low, high):
+    pivot = nums[high]
+    i = low
+    for currentIndex in range(low, high):
+        if nums[currentIndex] < pivot:
+            nums[currentIndex], nums[i] = nums[i], nums[currentIndex]
+            i += 1
+    
+    nums[i], nums[high] = nums[high], nums[i]
+    return i
+
     
---
+++ Course 9/Chapter 4/selectionSort.py
@@ -0,0 +1,4 @@
+#Lesson Link: https://www.boot.dev/lessons/e30cadd2-5610-4b47-bcf3-e54bb44d07a6
+#Starting file now but finishing lesson later
+def selection_sort(nums):
+    pass
