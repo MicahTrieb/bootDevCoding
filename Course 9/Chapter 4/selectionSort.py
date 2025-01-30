6aa89e0f960b5e21b48d430fcaf02e404ab8a0ca
lowrezdragon@gmail.com <lowrezdragon@gmail.com>
2025-01-30 12:42:36 -0600

    Finished selectionsort.py

--- Course 9/Chapter 4/selectionSort.py
+++ Course 9/Chapter 4/selectionSort.py
@@ -1,4 +1,17 @@
 #Lesson Link: https://www.boot.dev/lessons/e30cadd2-5610-4b47-bcf3-e54bb44d07a6
-#Starting file now but finishing lesson later
+#   
 def selection_sort(nums):
-    pass
+
+
+    for currentIndex in range (0, len(nums)):
+        for currentInnerIndex in range(currentIndex + 1, len(nums)):
+            if nums[currentIndex] > nums[currentInnerIndex]:
+                nums[currentIndex], nums[currentInnerIndex] = nums[currentInnerIndex], nums[currentIndex]
+
+    return(nums)
+                
+            
+
+                
+            
+
