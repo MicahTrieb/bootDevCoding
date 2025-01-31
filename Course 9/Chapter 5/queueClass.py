8e6a8219dcdbb205d44ad803a1c8660667a4bcb1
lowrezdragon@gmail.com <lowrezdragon@gmail.com>
2025-01-31 10:32:07 -0600

    queueClassd.py

---
+++ Course 9/Chapter 5/queueClass.py
@@ -0,0 +1,18 @@
+#Lesson Link: https://www.boot.dev/lessons/73289433-96a5-4468-a977-7bbe3e731767
+
+
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        pass
+
+    def pop(self):
+        pass
+
+    def peek(self):
+        pass
+
+    def size(self):
+        pass
