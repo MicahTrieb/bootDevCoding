355cfefdc583daf7bdfe89e40b59dc635c2b3441
lowrezdragon@gmail.com <lowrezdragon@gmail.com>
2025-02-10 12:39:36 -0600

    triesSearching.py

---
+++ Course 9/Chapter 13/triesSearching.py
@@ -0,0 +1,25 @@
+#Lesson Link: https://www.boot.dev/lessons/aa5dc9d4-5286-4adf-a8c5-03e738a781a6
+
+class Trie:
+    def exists(self, word):
+        currentDict = self.root
+        for currentLetter in word:
+            if currentLetter in currentDict:
+                if currentDict[currentLetter] == {"*": True}:
+                    return True
+                currentDict = currentDict[currentLetter]
+        return False
+
+    # don't touch below this line
+
+    def add(self, word):
+        current = self.root
+        for letter in word:
+            if letter not in current:
+                current[letter] = {}
+            current = current[letter]
+        current[self.end_symbol] = True
+
+    def __init__(self):
+        self.root = {}
+        self.end_symbol = "*"
