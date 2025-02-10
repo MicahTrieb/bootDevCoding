3924825f6c98af23dbeccc70d64314706adcc2a8
lowrezdragon@gmail.com <lowrezdragon@gmail.com>
2025-02-10 13:40:52 -0600

    More work on triePrefixes.py

--- Course 9/Chapter 13/triePrefixes.py
+++ Course 9/Chapter 13/triePrefixes.py
@@ -2,15 +2,20 @@ class Trie:
     def search_level(self, current_level, current_prefix, words):
         if self.end_symbol in current_level:
             words += current_prefix
-        allKeys = current_level.keys()
-        allKeys = allKeys.sorted()
+        allKeys = current_level.keys().sorted()
         for currentKey in allKeys:
-            if currentKey != self.end_symbol:
+            if currentKey is not self.end_symbol:
                 search_level(currentKey, current_prefix + currentKey, words)
+
         return words
     def words_with_prefix(self, prefix):
-        emptyList = []
-        
+        newList = []
+        copyDict = self
+        for currentLetter in prefix:
+            if currentLetter in copyDict:
+                copyDict = copyDict[currentLetter]
+        search_level(copyDict, prefix, newList)
+        return newList
 
     # don't touch below this line
 
