38e33bbeff4c727ecb1a7ec4ecded6b7de312e2f
lowrezdragon@gmail.com <lowrezdragon@gmail.com>
2025-02-11 16:38:57 -0600

    Working on longestPrefix.py

---
+++ Course 9/Chapter 13/longestPrefix.py
@@ -0,0 +1,19 @@
+#Lesson Link: https://www.boot.dev/lessons/f8a46c86-6c34-4f41-90f7-bb6f6a881b3c
+
+class Trie:
+    def longest_common_prefix(self):
+        pass
+
+    # don't touch below this line
+
+    def __init__(self):
+        self.root = {}
+        self.end_symbol = "*"
+
+    def add(self, word):
+        current = self.root
+        for letter in word:
+            if letter not in current:
+                current[letter] = {}
+            current = current[letter]
+        current[self.end_symbol] = True
