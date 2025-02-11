8028f749c6261ae5984d30612e2359596a1bc30a
lowrezdragon@gmail.com <lowrezdragon@gmail.com>
2025-02-11 15:07:25 -0600

    Redid trieTrees.py

--- Course 9/Chapter 13/trieTrees.py
+++ Course 9/Chapter 13/trieTrees.py
@@ -1,5 +1,29 @@
 #Lesson Link: https://www.boot.dev/lessons/820b7d02-52fd-4c94-8c62-d52242a9c438
 
+#Rewriten for practice
+
+class Trie:
+    def add(self, word):
+        tempVariable = self.root
+        for currentLetter in word:
+            if currentLetter not in tempVariable:
+                tempVariable[currentLetter] = {}
+            tempVariable = tempVariable[currentLetter]
+        tempVariable["*"] = True
+
+            
+    # don't touch below this line
+
+    def __init__(self):
+        self.root = {}
+        self.end_symbol = "*"
+
+
+
+
+
+
+
 class Trie:
     def add(self, word):
         currentValue = self.root
