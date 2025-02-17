4104d70e587c13247a156c2aaf0566432465c14f
lowrezdragon@gmail.com <lowrezdragon@gmail.com>
2025-02-17 14:26:13 -0600

    Pain

--- Course 9/Chapter 16/travelingSalesman.py
+++ Course 9/Chapter 16/travelingSalesman.py
@@ -1,5 +1,38 @@
 #Lesson Link: https://www.boot.dev/lessons/64877fbc-b634-4b11-8b8e-1ba6460adcf6
 
+
+def tsp(cities, paths, dist):
+    permutationList = permutations(paths)
+    totalSum = 0
+    for currentPermutation in permutationList:
+        
+
+# don't touch below this line
+
+
+def permutations(arr):
+    res = []
+    res = helper(res, arr, len(arr))
+    return res
+
+
+def helper(res, arr, n):
+    if n == 1:
+        tmp = arr.copy()
+        res.append(tmp)
+    else:
+        for i in range(n):
+            res = helper(res, arr, n - 1)
+            if n % 2 == 1:
+                arr[n - 1], arr[i] = arr[i], arr[n - 1]
+            else:
+                arr[0], arr[n - 1] = arr[n - 1], arr[0]
+    return res
+
+
+
+#---------------------------------------------------
+#Third official rewrite, I've lost track
 def tsp(cities, paths, dist):
     permutationList = permutations(paths)
     totalSum = 0
