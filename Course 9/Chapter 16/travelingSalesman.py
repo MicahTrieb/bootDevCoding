08ad9c59eadeeacad7ae621569ab2af8ad5764c4
lowrezdragon@gmail.com <lowrezdragon@gmail.com>
2025-02-17 14:02:32 -0600

    More work on traveling salesman

--- Course 9/Chapter 16/travelingSalesman.py
+++ Course 9/Chapter 16/travelingSalesman.py
@@ -1,5 +1,46 @@
 #Lesson Link: https://www.boot.dev/lessons/64877fbc-b634-4b11-8b8e-1ba6460adcf6
 
+def tsp(cities, paths, dist):
+    permutationList = permutations(paths)
+    totalSum = 0
+    for i in range(0, len(permutationList[0])):
+        currentMinimum = float("inf")
+        for currentPermutation in permutationList:
+            print(currentPermutation)
+            currentMinimum = min(currentMinimum, currentPermutation[i])
+        totalSum += currentMinimum
+    print (totalSum + dist)
+    if totalSum < dist:
+        return True
+    return False
+            
+            
+
+
+    #Grab every index, calculating minimum, add them up
+
+
+# don't touch below this line
+
+
+def permutations(arr):
+    res = []
+    res = helper(res, arr, len(arr))
+    return res
+
+
+def helper(res, arr, n):
+    if n == 1:
+        tmp = arr.copy()
+        res.append(tmp)
+    else:
+        for i in range(n):
+            res = helper(res, arr, n - 1)
+            if n % 2 == 1:
+                arr[n - 1], arr[i] = arr[i], arr[n - 1]
+            else:
+                arr[0], arr[n - 1] = arr[n - 1], arr[0]
+    return res
 
 
 
